030570a43e9bb746fa158876491570d78680317a
Dan Ahern <danieljdahern@gmail.com>
2024-08-07 12:09:16 +0100

    Initial commit to setup starter project hierarchy

---
+++ Services/HolidaySearchService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearchOTB.Services
+{
+    public class HolidaySearchService
+    {
+
+        // Load in flight and hotel data
+        // Parse user query
+        /* 1. Find user's departure airport from list of possible airports
+         * 2. For every possible airport, run a search to find all flights going to destination on specified date
+         * 3. From there, find all local hotels from list of possible local hotels that have the right amount of nights
+         * 4. Then, return these so they're accessible under the .Results operator
+         */
+    }
+}
