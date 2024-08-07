e4547944ea9edfb8d4235a162e225e94f97e5e7c
Dan Ahern <danieljdahern@gmail.com>
2024-08-07 12:02:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HolidaySearchOTB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
