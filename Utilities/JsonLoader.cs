43078fc7cdf0d53fd8f2da20a4bba65b649e72ab
Dan Ahern <danieljdahern@gmail.com>
2024-08-07 12:12:44 +0100

    Implemented JsonLoader utility to parse data from Json files

---
+++ Utilities/JsonLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace HolidaySearchOTB.Utilities
+{
+    public class JsonLoader
+    {
+        public List<T> LoadJsonData<T>(string filePath)
+        {
+            // Read in Json text and then return it deserialised
+            var jsonData = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<T>>(jsonData);
+        }
+    }
+}
